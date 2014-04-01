9e69349bd4f4fbc77f64b05d7e9cd432e76d5188
rabbit_fighter <email.void@gmail.com>
2014-04-01 15:48:32 -0700

    css update

--- inc/header.php
+++ inc/header.php
@@ -31,7 +31,8 @@ I.T. Inventory</title>
 <script type="text/javascript" src="inc/bootstrap3/js/bootstrap.min.js"></script>
 
 <!-- Bootstrap core CSS -->
-    <link href="inc/bootstrap3/css/bootstrap.css" rel="stylesheet">
+    <!--<link href="inc/bootstrap3/css/bootstrap.css" rel="stylesheet">-->
+    <link href='inc/bootstrap3/css/bootstrap_yeti.css' rel='stylesheet'>
     <link rel="stylesheet" href="inc/FontAwesome/css/font-awesome.min.css">
 
 </head>
@@ -39,7 +40,7 @@ I.T. Inventory</title>
 
 
 <!-- NAVBAR -->
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
 <div class="container">
 	<div class="navbar-header">
 	<a class="navbar-brand" href="index.php"><b>INVENTORY</b></a>
@@ -47,7 +48,7 @@ I.T. Inventory</title>
 	<div class="collapse navbar-collapse">
 	<ul class="nav navbar-nav">
 		<li><a href="item.php?action=add"><b>Add Item</b></a></li>
-		<li><a href="checked_out.php"><b>View Checked Out Items</b></a></li>
+		<li><a href="checked_out.php"><b>Loaned Items</b></a></li>
 		<li><a href="reports.php"><b>Reports</b></a></li>
 	</ul>
 	</div>
